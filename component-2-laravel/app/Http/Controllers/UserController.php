22ea3377b217d23c708f9c161fadd943995cf497
diwashbhatta17 <bhattaaawash@gmail.com>
2023-12-19 07:50:37 +0545

    Adding React frontend project in the vesion control

--- component-2-laravel/app/Http/Controllers/UserController.php
+++ component-2-laravel/app/Http/Controllers/UserController.php
@@ -60,6 +60,8 @@ class UserController extends Controller
         }
     }
 
+    
+
 
 
 }
---
+++ react-frontend-comp-2/src/App.js
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    <>
+    <div className="text-2xl text-red font-bold">Hello from Diwash Bhatta</div></>
+  );
+}
+
+export default App;
